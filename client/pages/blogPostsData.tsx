a3dd0a11e8d809b82e0f52765aca40ef0869cd58
acrubaugh11 <abcrubaugh@uncg.edu>
2025-09-13 17:06:28 -0400

    blog post data 1

--- client/pages/blogPostsData.tsx
+++ client/pages/blogPostsData.tsx
@@ -20,6 +20,24 @@ const blogPosts: BlogPost[] = [
     content: `
 
 
+I’ve chosen to write this piece as a reflection on the experiences I’ve had throughout the 18 years of my childhood, during which I had the privilege of growing up overseas across Africa.
+
+To begin with, a proper background and introduction are important.I’ve lived in four different countries across Africa: Guinea (Conakry), Dar es Salaam (Tanzania), Accra (Ghana), and Lusaka (Zambia). I spent between three to five years in each country, each during a different period of my life. The reason I lived in so many different countries is due to my father’s job with USAID, where employees often relocate to different posts across various locations. 
+
+From living across so many different countries I was able to meet and connect with very many different kinds of people, including those who were in similar positions to mine. In fact, the very motivation for this writing comes from a recent encounter from a very old friend who I once had grown up with in Accra (Ghana). After both moving apart and not having been in contact for a long time, it was not until we met once again much later that we got together to chat again. During our conversation it became apparent the similarities in thoughts we had towards growing up overseas. It was comforting to hear from someone in a similar situation, that I was not alone. So if you have also shared a similar experience, or perhaps are just interested, I hope this piece is able to convey some of the feelings from such experiences.
+
+Firstly, and most importantly, the opportunity to grow up overseas is an unforgettable 
+experience, and anyone who has the chance should definitely consider working,moving, or even visiting a country abroad for many reasons. Being immersed in a culture different to what you are used to encourages yourself to develop traits of open-mindedness and respect. At times it feels odd or uncomfortable trying to adapt to new social settings, but these experiences are what help you grow and broaden your perspective. Understanding that the place you are living has its own long-established way of life - that may be different to your own, yet equally valid - helps encourage respect and appreciation for others and their cultures. An example I can give that I experienced growing up around Africa was the behavioral nuances around respect given to elders. In certain areas like Nigeria and other South-Eastern African
+countries respect towards elders is highly valued. At times younger people are even expected to bow - or show some form of respect when in the presence of respected elders. This is just one of many nuances that highlight the  differences in social life across countries. As you begin to immerse yourself in a new environment and learn these cultural variations, you gain a much stronger and balanced perspective on life. You are not confined to values that are shaped by one single environment, instead you learn to appreciate diverse ways of living - and co-existing - which allows you to develop empathy, open-mindedness and adaptability.
+
+Conversely, growing up overseas can be difficult at times, being in unfamiliar environments - and multiple of them for long periods of time - can cause feelings of "outsiderhood". Outsiderhood is defined as the state or condition of being an 
+outsider. In my personal experience, I recall many different people I was able to make connections with, some were very strong, and others not so much. Yet, nonetheless they were connections I valued. Which is why having to leave after 4 or 5 years of developing said connections to somewhere new was always difficult. Having the chance to look back and reflect, I realize that moving in such a manner was no easy task. To have to let go of friends, connections, all the while finding yourself in a completely new environment you are not used to is a very humbling experience. Many times I felt as though I was an "outsider". Having been born in the USA yet grown up across Africa, I struggled to define where "home" was for me. Was it the last country I came from? Was it where I was born in the USA - yet had not lived in for very long? Or was it the place I returned to every summer, and had family waiting for me? If you ever ask someone "where are you from?", what sort of answer would they give you? Still to this day, I struggle to give a clear answer as to where "home" is or where "I am from". Yet 
+these experiences are an important part of me, and have taught me adaptability - to live in new environments, courage - to be able to continue moving, respect - to those cultures I was fortunate enough to experience, open-mindedness - to different ways of life, and the value of family - who will always be there no matter how much you move around.
+
+I am beyond blessed to have such a unique experience growing up. If at all you have shared similar experiences or opinions let me know!
+
+
+
     `,
   },
 
